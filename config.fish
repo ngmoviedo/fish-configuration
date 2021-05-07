4db2dfc57dbd181ec2918245952aacb49f1ec764
Nicolás González Meneses <ngmoviedo@gmx.com>
2021-05-07 16:49:43 +0200

    Mac support for budspencer prompt

--- config.fish
+++ config.fish
@@ -1,11 +1,10 @@
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty &> /dev/null
-    end
-end
-
 # Alias
 # thefuck
 thefuck --alias | source
+
+# Mac support
+if test (uname -s) = "Darwin"
+  set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+  set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+end
 
