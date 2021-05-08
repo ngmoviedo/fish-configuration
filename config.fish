6a5e9fd51c293c4445049ca4d3e929d47cf46c92
nicolas <ngmoviedo@gmx.com>
2021-05-08 13:27:05 +0200

    Linux and Mac cross-compatibility

--- config.fish
+++ config.fish
@@ -1,6 +1,11 @@
-# Alias
-# thefuck
-thefuck --alias | source
+# Start X at login (on Linux)
+if test (uname -s) = "Linux"
+    if status is-login
+        if test -z "$DISPLAY" -a $XDG_VTNR = 1
+            exec startx -- -keeptty &> /dev/null
+        end
+    end
+end
 
 # Mac support
 if test (uname -s) = "Darwin"
@@ -8,3 +13,6 @@ if test (uname -s) = "Darwin"
   set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
 end
 
+# Alias
+# thefuck
+thefuck --alias | source
